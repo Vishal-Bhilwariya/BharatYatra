85b85faa7d8b5aa227817fa84b5d7b4633df8653
Vishal <vishalbhilwariya@gmail.com>
2025-12-24 12:22:26 +0530

    Added place in city

--- server/controllers/cityController.js
+++ server/controllers/cityController.js
@@ -43,4 +43,27 @@ const getCityById = async (req, res) => {
   }
 };
 
-module.exports = { getCities, addCity, getCityById };
+// Get city with its places
+const getCityWithPlaces = async (req, res) => {
+  try {
+    const city = await City.findById(req.params.id);
+
+    if (!city) {
+      return res.status(404).json({ message: "City not found" });
+    }
+
+    // Find places of this city
+    const places = await require("../models/Place").find({
+      city: city._id,
+    });
+
+    res.json({
+      city,
+      places,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getCities, addCity, getCityById, getCityWithPlaces };
--- server/routes/cityRoutes.js
+++ server/routes/cityRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { getCities, addCity , getCityById} = require("../controllers/cityController");
+const { getCityWithPlaces } = require("../controllers/cityController");
 
 // GET all cities
 router.get("/", getCities);
 // POST add city
 router.post("/", addCity);
+// Get city with places
+router.get("/:id/places", getCityWithPlaces);
 // GET single city
 router.get("/:id", getCityById);
 module.exports = router;
