d3a24170a743de8e676d46c978367120d2b44e80
Vishal <vishalbhilwariya@gmail.com>
2025-12-24 11:46:50 +0530

    Added get single city API

--- server/controllers/cityController.js
+++ server/controllers/cityController.js
@@ -28,4 +28,19 @@ const addCity = async (req, res) => {
   }
 };
 
-module.exports = { getCities, addCity };
+// Get single city by ID
+const getCityById = async (req, res) => {
+  try {
+    const city = await City.findById(req.params.id);
+
+    if (!city) {
+      return res.status(404).json({ message: "City not found" });
+    }
+
+    res.json(city);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getCities, addCity, getCityById };
--- server/routes/cityRoutes.js
+++ server/routes/cityRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getCities, addCity } = require("../controllers/cityController");
+const { getCities, addCity , getCityById} = require("../controllers/cityController");
 
 // GET all cities
 router.get("/", getCities);
-
 // POST add city
 router.post("/", addCity);
-
+// GET single city
+router.get("/:id", getCityById);
 module.exports = router;
