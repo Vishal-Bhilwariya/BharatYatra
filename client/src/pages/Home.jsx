b1b99d347b3e88c6466169c1f30dbfa915d26a34
Vishal <vishalbhilwariya@gmail.com>
2025-12-25 11:03:54 +0530

    Initialize React frontend setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>BharatYatra</h1>
+      <p>Explore cities, culture, food, transport & language</p>
+    </div>
+  );
+};
+
+export default Home;
